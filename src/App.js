f62770526aed5cd68fb2b1d6126514158e24f71e
mehdibyt <mehdi.byt@live.com>
2019-12-24 10:27:23 +0330

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is first page
+    </div>
+  );
+}
+
+export default App;
